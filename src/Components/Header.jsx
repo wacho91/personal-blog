31715df0943ed162e19a08e9bd791baf2b459ad9
wacho91 <criskol.71@gmail.com>
2024-12-26 12:11:15 -0500

    Arquitecture Header Component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,23 @@
 
+import { IoLogoYoutube } from "react-icons/io5";
+import Logo from "../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+
+    const navigate = useNavigate();
+
   return (
-    <div>
-        Header Component 
+    <div className="flex justify-between items-center">
+        <img src={Logo} className="w-[180px]" alt="" /> 
+        <ul>
+            <li onClick={() => navigate('/')} >Hpme</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+        <button>
+            Subscribe <IoLogoYoutube />
+        </button>
     </div>
   )
 }
