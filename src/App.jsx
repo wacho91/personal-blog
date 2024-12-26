2587a538e0c2553117a4de9d1815064f31a9816a
wacho91 <criskol.71@gmail.com>
2024-12-26 11:58:53 -0500

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./Components/Header"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-[20px]">
+        <Header />
+    </div>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+        Header Component 
+    </div>
+  )
+}
+
+export default Header
