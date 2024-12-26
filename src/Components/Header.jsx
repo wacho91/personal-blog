06de0e328b45c5161e4121e230472d32a34f5e87
wacho91 <criskol.71@gmail.com>
2024-12-26 12:19:50 -0500

    Header Component Complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center">
         <img src={Logo} className="w-[180px]" alt="" /> 
-        <ul>
-            <li onClick={() => navigate('/')} >Hpme</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
+        <ul className="hidden md:flex gap-4 md:gap-14">
+            <li className="hover:font-bold cursor-pointer" onClick={()=>navigate('/')} >Home</li>
+            <li className="hover:font-bold cursor-pointer">About Us</li>
+            <li className="hover:font-bold cursor-pointer">Contact Us</li>
         </ul>
-        <button>
-            Subscribe <IoLogoYoutube />
+        <button className="bg-red-500 rounded-full text-white flex items-center text-[14px]">
+            Subscribe <IoLogoYoutube className='ml-3 text-[20px]' />
         </button>
     </div>
   )
