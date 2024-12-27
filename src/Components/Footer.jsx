b58775036b5de5a72c334c193864a48c9e3ea026
wacho91 <criskol.71@gmail.com>
2024-12-27 11:20:12 -0500

    Footer component complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-200 text-center'>
+        <h1 className='text-[14px] p-10'>Need help? Email hello@tubeguruji.com<br></br>
+        Copyright © 2024 Tubeguruji</h1>
+    </div>
+  )
+}
+
+export default Footer
