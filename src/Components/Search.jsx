48437c0a409a51633ee4cdee116f678c18c945be
wacho91 <criskol.71@gmail.com>
2024-12-30 10:40:35 -0500

    Arquitecture component complete

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react"
+import Banner from '../assets/images/banner.jpg';
+import {  IoSearchOutline } from "react-icons/io5";
 
-const Search = () => {
+const Search = ({selectedTag}) => {
+
+    const tags=[
+        {
+            id:1,
+            name:'All',
+        },
+        {
+            id:2,
+            name:'React',
+        },
+        {
+            id:3,
+            name:'React Native',
+        },
+        {
+            id:4,
+            name:'Angular',
+        },
+        {
+            id:5,
+            name:'UI/UX',
+        },
+    ]
+
+    const [activeIndex,setActiveIndex]=useState(0);
+
   return (
     <div>
-        Search Component
+        <img src={Banner} alt="" />
+        <div>
+            <IoSearchOutline />
+            <input type="text" placeholder="Search" />
+        </div>
+        <div>
+            {tags.map((item, index) => (
+                <ul 
+                    key={item.id}
+                    onClick={()=>{setActiveIndex(index);selectedTag(item.name)}} 
+                >
+                    <li>{item.name}</li>
+                </ul>
+            ))}
+        </div>
     </div>
   )
 }
