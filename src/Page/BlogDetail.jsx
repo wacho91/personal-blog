dd3b98d82f2f621959460dc60247677ddb5d964d
wacho91 <criskol.71@gmail.com>
2024-12-30 11:04:56 -0500

    Arquitecture Component

--- src/Page/BlogDetail.jsx
+++ src/Page/BlogDetail.jsx
@@ -1,9 +1,49 @@
-
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import GlobalApi from "../Services/GlobalApi"
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 const BlogDetail = () => {
+
+    const {id} = useParams();
+    const [post, setPost] = useState([]);
+
+    useEffect(() => {
+        getBlogById();
+    })
+
+    const getBlogById=()=>{
+        GlobalApi.getPostById(id).then(resp=>{
+           
+            const item=resp.data.data;
+            const result={
+                id:item.id,
+                title:item.attributes.title,
+                desc:item.attributes.description,
+                tag:item.attributes.tag,
+                coverImage:item.attributes.coverImage.data.attributes.url,
+            };
+            setPost(result);
+            console.log("Result",result);
+        })
+    }
+
   return (
     <div>
-      
+      <h3>{post.tag}</h3>
+      <h3>{post.title}</h3>
+      <div>
+        <img 
+            src="https://courses.tubeguruji.com/static/media/logo.8f2db318fe31ffaf5793.png"
+            className='w-[35px] rounded-full'
+        />
+        <div>
+            <h3>Tubeguruji</h3>
+            <h3>24 Sept 2024</h3>
+        </div>
+      </div>
+      <img src={post.coverImage} className='rounded-2xl mt-5 mb-5 w-full'/>
+      <ReactMarkdown />
     </div>
   )
 }
