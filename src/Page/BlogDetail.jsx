8c7346d90eea594425e7313954ed8f6c90dff2a8
wacho91 <criskol.71@gmail.com>
2024-12-30 11:09:59 -0500

    BlogDetail component Complete

--- src/Page/BlogDetail.jsx
+++ src/Page/BlogDetail.jsx
@@ -29,21 +29,21 @@ const BlogDetail = () => {
     }
 
   return (
-    <div>
-      <h3>{post.tag}</h3>
-      <h3>{post.title}</h3>
-      <div>
+    <div className='px-6 md:px-20 lg:px-56 mt-10'>
+      <h3 className='text-red-500 text-[12px]'>{post.tag}</h3>
+      <h3 className='text-[23px] font-bold'>{post.title}</h3>
+      <div className='flex items-center mt-5'>
         <img 
             src="https://courses.tubeguruji.com/static/media/logo.8f2db318fe31ffaf5793.png"
             className='w-[35px] rounded-full'
         />
-        <div>
-            <h3>Tubeguruji</h3>
-            <h3>24 Sept 2024</h3>
+        <div className='ml-2'>
+            <h3 className='font-bold text-[12px]'>Tubeguruji</h3>
+            <h3 className='text-gray-500 text-[10px]'>24 Sept 2024</h3>
         </div>
       </div>
       <img src={post.coverImage} className='rounded-2xl mt-5 mb-5 w-full'/>
-      <ReactMarkdown />
+      <ReactMarkdown  children={post.desc} escapeHtml={false} className='leading-9'/>
     </div>
   )
 }
