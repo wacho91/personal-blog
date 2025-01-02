0bab61695c62585b4f8d66e5cfb6aa8aecd85dfe
wacho91 <criskol.71@gmail.com>
2025-01-02 11:40:42 -0500

    Changes in the blogdetail component

--- src/Page/BlogDetail.jsx
+++ src/Page/BlogDetail.jsx
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import GlobalApi from "../Services/GlobalApi"
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import GlobalApi from '../Services/GlobalApi';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
-const BlogDetail = () => {
-
-    const {id} = useParams();
-    const [post, setPost] = useState([]);
-
-    useEffect(() => {
+function BlogDetail() {
+    const {id}=useParams();
+    const [post,setPost]=useState([])
+    useEffect(()=>{
+        console.log("Id",id)
         getBlogById();
-    })
+    },[])
 
     const getBlogById=()=>{
         GlobalApi.getPostById(id).then(resp=>{
@@ -27,23 +26,22 @@ const BlogDetail = () => {
             console.log("Result",result);
         })
     }
-
   return (
     <div className='px-6 md:px-20 lg:px-56 mt-10'>
-      <h3 className='text-red-500 text-[12px]'>{post.tag}</h3>
-      <h3 className='text-[23px] font-bold'>{post.title}</h3>
-      <div className='flex items-center mt-5'>
-        <img 
-            src="https://courses.tubeguruji.com/static/media/logo.8f2db318fe31ffaf5793.png"
-            className='w-[35px] rounded-full'
-        />
-        <div className='ml-2'>
-            <h3 className='font-bold text-[12px]'>Tubeguruji</h3>
-            <h3 className='text-gray-500 text-[10px]'>24 Sept 2024</h3>
+        <h3 className='text-red-500 text-[12px]'>{post.tag}</h3>
+        <h3 className='text-[23px] font-bold'>{post.title}</h3>
+        <div className='flex items-center mt-5'>
+            <img src="https://courses.tubeguruji.com/static/media/logo.8f2db318fe31ffaf5793.png"
+            className='w-[35px] rounded-full'/>
+            <div className='ml-2'>
+                <h3 className='font-bold text-[12px]'>Tubeguruji</h3>
+                <h3 className='text-gray-500 text-[10px]'>24 Sept 2024</h3>
+            </div>
         </div>
-      </div>
-      <img src={post.coverImage} className='rounded-2xl mt-5 mb-5 w-full'/>
-      <ReactMarkdown  children={post.desc} escapeHtml={false} className='leading-9'/>
+        <img src={post.coverImage} className='rounded-2xl mt-5 mb-5 w-full'/>
+        {/* <h3>{post.desc}</h3> */}
+        <ReactMarkdown children={post.desc} 
+        escapeHtml={false} className='leading-9' />
     </div>
   )
 }
