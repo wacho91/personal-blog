60359af113d0979531a4a2e5086203262259060b
wacho91 <criskol.71@gmail.com>
2025-01-02 11:35:40 -0500

    App complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
+import Home from "./Page/Home"
+import BlogDetail from "./Page/BlogDetail"
 
 function App() {
 
@@ -10,7 +12,8 @@ function App() {
       <div className="p-[20px]">
           <Header />
           <Routes>
-
+              <Route path="/" element={<Home />} />
+              <Route path="/blog-detail/:id" element={<BlogDetail />} />
           </Routes>
       </div>
       <Footer /> 
